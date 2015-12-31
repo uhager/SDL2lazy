f0ab592e3f99641d8918ddf81e3b2fc633649f06
uhager <uhager42@gmail.com>
2015-12-31 12:59:22 -0500

    Updated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setcolor',['setColor',['../classSlTexture.html#af454913583e879c722dc83e695b33066',1,'SlTexture']]],
+  ['setcurrentsprite',['setCurrentSprite',['../classSlSpritesheet.html#a137e0e7bc5e16ac36f481a3e94cfe371',1,'SlSpritesheet']]],
   ['setdestination',['setDestination',['../classSlSpritesheet.html#aa1f92a733875563d3ee82b5023fd1c2c',1,'SlSpritesheet::setDestination()'],['../classSlTexture.html#a37c0bf8da05161a832888d6c1b9b5884',1,'SlTexture::setDestination()']]],
   ['setdestinationdimension',['setDestinationDimension',['../classSlSpritesheet.html#a234b1c3fe12f4eba8b89bc3514d9cb8f',1,'SlSpritesheet::setDestinationDimension()'],['../classSlTexture.html#a285da30b12e0f2bd55d987038270f351',1,'SlTexture::setDestinationDimension()']]],
   ['setdestinationorigin',['setDestinationOrigin',['../classSlSpritesheet.html#a5e6e20eaf6ce0e0e461cb52aac5fdded',1,'SlSpritesheet::setDestinationOrigin()'],['../classSlTexture.html#ac734b57da49659cc4e1f02ed77896390',1,'SlTexture::setDestinationOrigin()']]],
